377d52c7137bf9c6ea21fc4e3fb6e5c87cf64c5f
Ravis Ratesey <162020276+DrRavis@users.noreply.github.com>
2024-11-30 19:53:54 +0300

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -1,8 +1,7 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for word in other_words:
-        # if word.lower() in root_word.lower() or root_word.lower() in word.lower():
-        if word.lower().count(root_word.lower()) == 1 or root_word.lower().count(word.lower()) == 1:
+        if word.lower() in root_word.lower() or root_word.lower() in word.lower():
             same_words.append(word)
         else:
             continue
